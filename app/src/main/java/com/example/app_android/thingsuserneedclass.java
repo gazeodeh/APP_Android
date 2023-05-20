ad562b86c25b33fa97ce3386a154d0f06136b050
mariamsleman1 <mariamsleman07@gmail.com>
2023-05-20 16:59:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/app_android/thingsuserneedclass.java
@@ -0,0 +1,19 @@
+package com.example.app_android;
+
+public class thingsuserneedclass {
+    private String first;
+    private String second;
+    private String third;
+    private String fourth;
+
+    public thingsuserneedclass(String first, String second, String third, String fourth) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/app_android/thingtheuserneedFragment.java
@@ -0,0 +1,125 @@
+package com.example.app_android;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link thingtheuserneedFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class thingtheuserneedFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private EditText First;
+    private EditText second;
+    private EditText third;
+    private EditText fourth;
+    private Button remidbtn;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public void func(){
+        String etfirst,etsecond,etthird,etfourth;
+        First=getView().findViewById(R.id.firstthing);
+        second=getView().findViewById(R.id.secindthing);
+        third=getView().findViewById(R.id.thirdthing);
+        fourth=getView().findViewById(R.id.fourththing);
+        remidbtn=getView().findViewById(R.id.remindmebtn);
+
+           etfirst=First.getText().toString();
+           etsecond=second.getText().toString();
+           etthird=third.getText().toString();
+           etfourth=fourth.getText().toString();
+
+           if (etfirst==null&&etsecond==null&&etthird==null&&etfourth==null){
+               Toast.makeText(getContext(), "somthing null!!", Toast.LENGTH_SHORT).show();
+           }
+           else {
+
+
+               thingsuserneedclass thingsuserneed=new thingsuserneedclass(etfirst,etsecond,etthird,etfourth);
+
+               db.collection("users")
+                       .add(per).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                           @Override
+                           public void onSuccess(DocumentReference documentReference) {
+                               Toast.makeText(thingsuserneed.this, "data was saved", Toast.LENGTH_SHORT).show();
+
+                               //go to projects
+                           }
+                       }).addOnFailureListener(new OnFailureListener() {
+                           @Override
+                           public void onFailure(@NonNull Exception e) {
+
+                               Toast.makeText(thingsuserneed.this, "data not saved try another data", Toast.LENGTH_SHORT).show();
+                               //try to save
+                               Log.e("",e.getMessage());
+                           }
+                       });
+
+           }
+
+
+
+    }
+
+
+
+
+
+
+    public thingtheuserneedFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment thingtheuserneedFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static thingtheuserneedFragment newInstance(String param1, String param2) {
+        thingtheuserneedFragment fragment = new thingtheuserneedFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_thingtheuserneed, container, false);
+    }
+}
